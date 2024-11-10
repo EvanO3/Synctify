2e39f7fc34b9e1622e6d7cb30c7ae5a11f542c8f
”EvanO3” <evan.oni@hotmail.com>
2024-11-10 00:32:44 -0500

    added search bar and icons to left bar

--- Synctify-FE/src/Component/LeftBar.jsx
+++ Synctify-FE/src/Component/LeftBar.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { GiSoundWaves } from "react-icons/gi";
+import { RiPlayListAddLine } from "react-icons/ri";
+import { AiOutlineHome } from "react-icons/ai";
+import { IoGridOutline } from "react-icons/io5";
+import { SlUserFollow } from "react-icons/sl";
+import { CiSearch } from "react-icons/ci";
 import "../Styles/Header.css"
 function LeftBar({ button, profile }) {
   return (
@@ -10,13 +15,33 @@ function LeftBar({ button, profile }) {
       </div>
 
       <nav className="left-nav">
+        <div className="input-container">
+          <CiSearch className="input-icon" colour={"White"} />
+          <input className="Search-bar" type="text" placeholder="Search..." />
+        </div>
         <ul>
-          <li>Home</li>
-          <li>New</li>
-          <li>Create Playlist</li>
-          <li>Follow Friend</li>
+          <li>
+            <AiOutlineHome color={"White"} size={"18px"} />
+            Home
+          </li>
+          <li>
+            <IoGridOutline color={"White"} />
+            New
+          </li>
+          <li>
+            <RiPlayListAddLine color={"White"} />
+            Create Playlist
+          </li>
+          <li>
+            <SlUserFollow color={"White"} />
+            Follow Friend
+          </li>
         </ul>
       </nav>
+
+      <footer className="left-footer">
+        <p> &copy; 2024 Synctify</p>
+      </footer>
     </div>
   );
 }
